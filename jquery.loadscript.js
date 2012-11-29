be37620f049518e15a8a992fa588a96573d58453
Florian Margaine <florian@margaine.com>
2012-11-29 10:10:29 +0100

    No need to worry about cross-browser
    jQuery already takes care of everything :-)

--- jquery.loadscript.js
+++ jquery.loadscript.js
@@ -8,30 +8,7 @@
  * v0.1.0 - 01/11/2012:
  * First release
  */
-;(function($){
-	// lazyload script
-	// ref: http://www.nczonline.net/blog/2009/07/28/the-best-way-to-load-external-javascript/
-	var _loadScript = function(url, callback){
-		var script = document.createElement("script")
-		script.type = "text/javascript";
-	 
-		if (script.readyState){ //IE
-			script.onreadystatechange = function(){
-			if ( script.readyState == "loaded" || script.readyState == "complete"){
-				script.onreadystatechange = null;
-				callback();
-			}
-		};
-		} else { //Others
-			script.onload = function(){
-				callback();
-			};
-		}
-	 
-		script.src = url;
-		document.getElementsByTagName("head")[0].appendChild(script);
-	};
-	
+;(function($){	
 	$.loadScript = function( p_url, p_callback ){
 		var _return = $.Deferred();
 		
@@ -43,7 +20,7 @@
 		}
 		
 		// load javascript file
-		_loadScript( p_url, function(){
+		$.getScript( p_url, function(){
 			_return.resolve();
 		} );
 		
